98bcb53b3bd924262d6d27ca984ed96a9817e8b6
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-06-21 22:11:04 -0300

    Update ProyectoIntegradorPython.py

--- ProyectoIntegradorPython.py
+++ ProyectoIntegradorPython.py
@@ -1,4 +1,53 @@
+#---------------------------------PUTRINO AGUSTIN----------------------------------------
+#Parte 1: Menú de ingreso de compras y verificiacion de cantidad 
 
+import sys
+
+def menuIngresoDeCompras():
+    calculoCompra = 0
+    cantidadArticulos = 0
+    i = 0
+    precioUnitario = 0
+    validarCantidad = False
+    validarPrecio = False
+
+    print("\n       <<<<<<<< DETALLE DEL TOTAL DE TU COMPRA >>>>>>>>>  \n")
+    while not validarCantidad:
+        try:
+            cantidadArticulos = int(input("  * CANTIDAD DE ARTÍCULOS QUE LLEVAS: "))
+            validarCantidad = validacionCantidadArticulos(cantidadArticulos)  # Validar la cantidad de artículos ingresada
+        except ValueError:
+            print("__________________________________")
+            print("    ERROR: CANTIDAD NO VÁLIDA")
+            print("__________________________________\n")
+
+    i = 0
+    calculoCompra = 0
+
+    while i != cantidadArticulos:
+        try:
+            precioUnitario = float(input(f"  * PRECIO DEL ARTÍCULO {i+1}°: "))
+            validarPrecio = validacionPrecioArticulo(precioUnitario)  # Validar el precio del artículo ingresado
+        except ValueError:
+            print("__________________________________")
+            print("      ERROR: PRECIO NO VÁLIDO")
+            print("__________________________________\n")
+        
+        if validarPrecio:
+            calculoCompra += precioUnitario
+            i += 1
+
+    print("____________________________________________________")
+    print(f"   EL IMPORTE TOTAL DE TU COMPRA ES ${calculoCompra}")
+    print("____________________________________________________")
+
+    menuDeConfirmacion(calculoCompra)  # Llamar a la función del menú de confirmación
+
+def validacionCantidadArticulos(cantidad):
+    return cantidad > 0  # Validar que la cantidad de artículos sea mayor a 0
+
+def validacionPrecioArticulo(precio):
+    return precio > 0  # Validar que el precio del artículo sea mayor a 0
 
 
 
